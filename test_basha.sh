fa00d2a7c4be12d73822226d3e6f11f0bfbacf4e
konstantinos <konstantinos.trochidis@gmail.com>
2021-01-14 05:48:59 +0000

    text file

---
+++ test_basha.sh
@@ -0,0 +1,3 @@
+for file in *.wav; do
+{ deepspeech --model /AI_module/output_graph.pb --alphabet /AI_module/alphabet.txt --lm /AI_module/lmodel.binary --trie /AI_module/trie --audio $file ; printf " " ; printf $file ; printf '\n' ; } >> text.txt
+done
