cf4b08a20a746b3a06f5fb311282b25e784d7428
konstantinos <konstantinos.trochidis@gmail.com>
2021-01-14 06:29:39 +0000

    New file

--- test_basha.sh
+++
@@ -1,3 +0,0 @@
-for file in *.wav; do
-{ deepspeech --model /AI_module/output_graph.pb --alphabet /AI_module/alphabet.txt --lm /AI_module/lmodel.binary --trie /AI_module/trie --audio $file ; printf " " ; printf $file ; printf '\n' ; } >> text.txt
-done
